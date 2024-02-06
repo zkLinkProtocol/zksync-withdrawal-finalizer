52d79f4883ea91625833ff6346d5f69e94a5a7f2
kevin <kevinfitzroy715@gmail.com>
2024-02-06 21:15:40 +0800

    fix: deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,26 +1,29 @@
-import { Wallet } from 'ethers';
+import { Wallet } from "ethers";
 
-const hardhat = require('hardhat');
+const hardhat = require("hardhat");
 
 async function main() {
-    const provider = new hardhat.ethers.providers.JsonRpcProvider(process.env.ETH_CLIENT_WEB3_URL as string);
-    const wallet  = Wallet.fromMnemonic(
-          process.env.MNEMONIC as string,
-          "m/44'/60'/0'/0/1"
-      ).connect(provider);
+  const provider = new hardhat.ethers.providers.JsonRpcProvider(
+    process.env.ETH_CLIENT_WEB3_URL as string
+  );
+  const wallet = Wallet.fromPhrase(process.env.MNEMONIC as string).connect(
+    provider
+  );
 
+  const contractFactory = await hardhat.ethers.getContractFactory(
+    "WithdrawalFinalizer",
+    {
+      signer: wallet,
+    }
+  );
+  const contract = await contractFactory.deploy();
+  await contract.deployTransaction.wait();
+  console.log(`CONTRACTS_WITHDRAWAL_FINALIZER_ADDRESS=${contract.address}`);
+}
 
-      const contractFactory = await hardhat.ethers.getContractFactory("WithdrawalFinalizer", {
-          signer: wallet
-      });
-      const contract = await contractFactory.deploy();
-      await contract.deployTransaction.wait();    
-      console.log(`CONTRACTS_WITHDRAWAL_FINALIZER_ADDRESS=${contract.address}`);
-  }
-  
-  main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
