73e20ff4daae9e30ce1786feffbb521fda240398
RequiemOfSouls <RequiemOfSouls@zklink.org>
2024-02-29 11:32:35 +0800

    fix: incorrect default id

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -685,7 +685,7 @@ pub async fn get_withdrawals_with_no_data(
               FROM
                 finalization_data
             ),
-            1
+            0
           )
           AND finalizable = TRUE
         ORDER BY
